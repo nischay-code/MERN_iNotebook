0b11de0f2eb4cfda55b8ee5eef44519ad0f49aa1
nischay-code <nischay.patel.code@gmail.com>
2024-01-21 21:54:47 +0530

    Storing Data into the Database Using Mongoose Model

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  let obj = { a: "This", number: 13 };
-  res.json(obj);
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
 });
 
 module.exports = router;
