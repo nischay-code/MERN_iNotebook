a26f2e354d5f0d7d61c33a479644060e6d7d448d
nischay-code <nischay.patel.code@gmail.com>
2024-01-23 12:12:01 +0530

    Updating an existing Note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -51,4 +51,42 @@ router.post(
   }
 );
 
+// ROUTE 3
+
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  try {
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    let note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+
+    // if (note.user.toString() !== req.user.id) {
+    //   return res.status(401).send("Not Allowed");
+    // }
+
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.json({ note });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 module.exports = router;
