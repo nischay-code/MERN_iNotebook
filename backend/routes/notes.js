7c8c82882c0ce3bb6259e9aa2a7c356bf88e092e
nischay-code <nischay.patel.code@gmail.com>
2024-01-23 18:17:42 +0530

    Endpoint for deleting a Note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -89,4 +89,26 @@ router.put("/updatenote/:id", fetchuser, async (req, res) => {
   }
 });
 
+// ROUTE 4
+
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+  try {
+    let note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+
+    if (note.user.toString() !== req.user) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Note has been deleted", note: note });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 module.exports = router;
