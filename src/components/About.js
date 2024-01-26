12b2db8b5c72d2936ff085e3862a652892e09391
nischay-code <nischay.patel.code@gmail.com>
2024-01-26 12:26:22 +0530

    Modification ☑️

--- src/components/About.js
+++ src/components/About.js
@@ -14,49 +14,49 @@ const About = () => {
       </div>
       <h3>Key Features:</h3>
       <p>
-        1. <b>Cloud-Powered Sync: iNotebook effortlessly syncs your notes</b>
+        1. <b>Cloud-Powered Sync:</b> iNotebook effortlessly syncs your notes
         across all your devices in real-time. Start a note on your laptop, tweak
-        it on your tablet, and finalize it on your phone – all without missing a
+        it on your tablet, and finalize it on your phone all without missing a
         beat.
       </p>
       <p>
-        2. <b>Secure Cloud Storage:Your thoughts are precious, and so is</b>
+        2. <b>Secure Cloud Storage:</b> Your thoughts are precious, and so is
         your data. iNotebook provides a secure cloud environment, ensuring your
         notes are safely stored and easily accessible whenever and wherever you
         need them.
       </p>
       <p>
-        3. <b>Organized Note-Taking: Whether it's meeting minutes, project</b>
+        3. <b>Organized Note-Taking:</b> Whether it's meeting minutes, project
         ideas, or personal reflections, iNotebook helps you keep everything
         organized. Tag, categorize, and search effortlessly to find what you
         need in an instant.
       </p>
       <p>
-        4. <b>Collaboration Made Easy:Collaborate seamlessly with colleagues</b>
-        or friends by sharing notebooks or specific notes. iNotebook facilitates
-        teamwork and idea-sharing, making it an ideal platform for group
-        projects.{" "}
+        4. <b>Collaboration Made Easy:</b> Collaborate seamlessly with
+        colleagues or friends by sharing notebooks or specific notes. iNotebook
+        facilitates teamwork and idea-sharing, making it an ideal platform for
+        group projects.
       </p>
       <p>
-        5. <b>Customizable Experience:Tailor iNotebook to suit your</b>
+        5. <b>Customizable Experience:</b> Tailor iNotebook to suit your
         preferences. Choose themes, adjust fonts, and personalize your workspace
         to create an environment that inspires your creativity.
       </p>
       <h2>Why iNotebook:</h2>
       <p>
-        *<b>Effortless Accessibility:</b>
-        Access your notes from anywhere with an internet connection. Your
-        digital notebook is always at your fingertips.
+        a. <b>Effortless Accessibility:</b> Access your notes from anywhere with
+        an internet connection. Your digital notebook is always at your
+        fingertips.
       </p>
       <p>
-        *<b>Future-Proof Note-Taking:</b>
-        iNotebook adapts to your needs, ensuring a future-proof solution for
-        your evolving note-taking requirements.
+        b. <b>Future-Proof Note-Taking:</b> iNotebook adapts to your needs,
+        ensuring a future-proof solution for your evolving note-taking
+        requirements.
       </p>
       <p>
-        *<b>User-Friendly Interface:</b>
-        Intuitive design makes iNotebook accessible to users of all levels. No
-        steep learning curves – just straightforward note-taking.
+        c. <b>User-Friendly Interface:</b> Intuitive design makes iNotebook
+        accessible to users of all levels. No steep learning curves – just
+        straightforward note-taking.
       </p>
     </div>
   );
